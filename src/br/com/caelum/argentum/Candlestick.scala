38a4b0ca1ec81a6ed74d92c34c157905debf2c54
Leonardo Cordeiro <leonardo.cordeiro@caelum.com.br>
2016-08-09 22:57:44 -0300

    mudando o isBaixa somente negando o isAlta

--- src/br/com/caelum/argentum/Candlestick.scala
+++ src/br/com/caelum/argentum/Candlestick.scala
@@ -6,9 +6,9 @@ import java.text.SimpleDateFormat
 class Candlestick(val abertura: Double, val fechamento: Double, val minimo: Double, val maximo: Double, val volume: Double, val data: Calendar) {
   
   def isAlta = abertura < fechamento
-  def isBaixa = abertura > fechamento
+  def isBaixa = !isAlta
   
-  override def toString() = {
+  override def toString = {
     val sdf = new SimpleDateFormat("dd/MM/yyyy")
     s"[Abertura ${abertura}, Fechamento ${fechamento}, Mínima ${minimo}, Máxima ${maximo}, Volume ${volume}, Data ${sdf.format(data.getTime)}]"
   }
